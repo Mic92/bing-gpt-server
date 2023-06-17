58cc2d9b0339820803c4f0ecd2ee7af83bc98694
Jörg Thalheim <joerg@thalheim.io>
2023-06-17 20:07:34 +0200

    Revert "edge-gpt: 0.10.14 -> 0.11.1"
    This reverts commit 3b3e00e96e039c0c4e0d3a5c287e01331ca31b4b.

--- edge-gpt/default.nix
+++ edge-gpt/default.nix
@@ -7,14 +7,14 @@
 
 python3.pkgs.buildPythonPackage rec {
   pname = "edge-gpt";
-  version = "0.11.1";
+  version = "0.10.14";
   format = "setuptools";
 
   src = fetchFromGitHub {
     owner = "acheong08";
     repo = "EdgeGPT";
     rev = version;
-    hash = "sha256-15XBS31uu5bmriYyn7N5BBLMAOjhj2uhcFmrJFwoejA=";
+    hash = "sha256-/dHHNLoPfngG64oD2MIZ7JnG0KtZB7KF5ZN4dPcDq9Y=";
   };
 
   postPatch = ''
